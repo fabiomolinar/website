9f4ba3a4a1a88fe4d04c3e73f13332b3f9f7046e
Fabio Thomaz Molinar <fabiomolinar@gmail.com>
2018-12-28 09:13:09 +0100

    Changing Ali app admin interface.

--- website/ali/admin.py
+++ website/ali/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Search
 
-# Register your models here.
+class SearchAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date_created'
+    list_display = ('search_text','results','currency','min_price','median','max_price','date_created')
+
+admin.site.register(Search, SearchAdmin)
