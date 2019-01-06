ae69e5cb18d639ec3cbf5526eacf65256c217f37
fabiomolinar <fabiomolinar@gmail.com>
2019-01-06 22:28:52 +0100

    Updating deploy tasks.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 # deploying to production
 
-# copy 
+# On droplet ---------------------
+# Copy/create .env file on website/env_files folder.
 
-# use *.prod files to overwrite files with production data
+# On website container -----------
+# If django superuser don't exist, create one.
+# Run migrations from within website
+python manage.py migrate
+python manage.py migrate ali --database=ali
+# Generate translation files
+python manage.py compilemessages
 
 # create cron job to run certbot every 15 days to update certificates
